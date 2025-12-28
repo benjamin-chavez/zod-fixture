ac2e2f2cddc4d821350c123bfb2409480fca4f61
Ben Chavez <ben.m.chavez@gmail.com>
2025-12-28 15:48:38 -0600

    fix: add manual type declarations

--- dist/public.d.ts
+++ dist/public.d.ts
@@ -1,1 +1,48 @@
-export * from './public'
+export interface Defaults {
+  seed?: number;
+  array: { min: number; max: number };
+  map: { min: number; max: number };
+  set: { min: number; max: number };
+  string: { min: number; max: number };
+  int: { min: number; max: number };
+  float: { min: number; max: number };
+  bigint: { min: bigint; max: bigint };
+  date: { min: Date; max: Date };
+}
+
+export declare function createFixture<TOutput = unknown>(
+  schema: { _output: TOutput } | { _zod: { _output: TOutput } } | Record<string, any>,
+  instanceDefaults?: Partial<Defaults>
+): TOutput;
+
+export declare class Fixture {
+  constructor(instanceDefaults?: Partial<Defaults>);
+  fromSchema<TOutput = unknown>(
+    schema: { _output: TOutput } | { _zod: { _output: TOutput } } | Record<string, any>,
+    instanceDefaults?: Partial<Defaults>
+  ): TOutput;
+  extend(...generators: Generator[]): this;
+}
+
+export declare class ConstrainedFixture extends Fixture {}
+export declare class UnconstrainedFixture extends Fixture {}
+
+export interface Generator {
+  schema?: any;
+  filter?: (args: { def: any; schema: any; transform: any; context: any }) => boolean;
+  output: (args: { def: any; schema: any; transform: any; context: any }) => any;
+}
+
+export declare function Generator(config: Generator): Generator;
+
+export declare class Transformer {
+  constructor(instanceDefaults?: Partial<Defaults>);
+  fromSchema<TOutput = unknown>(
+    schema: { _output: TOutput } | { _zod: { _output: TOutput } } | Record<string, any>,
+    instanceDefaults?: Partial<Defaults>
+  ): TOutput;
+  extend(...generators: Generator[]): this;
+}
+
+export declare class ConstrainedTransformer extends Transformer {}
+export declare class UnconstrainedTransformer extends Transformer {}
